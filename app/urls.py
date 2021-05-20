693e7acf434aa6b2d71b1a0f8ccb6061c234e554
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-05-20 14:07:47 +0300

    add ja delete tehty supp ja prod kenttiin

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import landingview, supplierlistview, addsupplier
+from .views import landingview, supplierlistview, addsupplier, deletesupplier, productslistview, addproduct, deleteproduct
 
 urlpatterns = [
     path('landing/', landingview),
     path('suppliers/', supplierlistview),
     path('add-supplier/', addsupplier),
-
+    path('delete-supplier/<int:id>/', deletesupplier),
+    path('products/', productslistview),
+    path('add-product/', addproduct),
+    path('delete-product/<int:id>', deleteproduct),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app.models import Supplier
 from django.shortcuts import render, redirect
-from .models import Supplier
+from .models import Product, Supplier
 
 def landingview(request):
     return render (request, "landingpage.html")
@@ -18,4 +18,30 @@ def addsupplier(request):
     e = request.POST['email']
     f = request.POST['country']
     Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+def deletesupplier(request, id):
+    Supplier.objects.filter(id = id).delete()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+# PRODUCT
+def productslistview(request):
+    productslist = Product.objects.all()
+    supplierlist = Supplier.objects.all()
+    context = {'products': productslist, 'suppliers': supplierlist}
+    return render (request,"products.html",context)
+
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['packagesize']
+    c = request.POST['unitprice']
+    d = request.POST['unitsinstock']
+    e = request.POST['supplier']
+    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier =
+    Supplier.objects.get(id=e)).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+def deleteproduct(request, id):
+    Product.objects.filter(id = id).delete()
     return redirect(request.META['HTTP_REFERER'])

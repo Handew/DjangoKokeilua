11f241551e1993abc64cef8d620e77e1963adb35
Hannu Ilvonen <hannu.ilvonen@gmail.com>
2021-05-27 13:32:58 +0300

    muutama testi lisätty

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase, Client
+from django.urls import reverse
+import unittest
+from .laskin import plus
+from .models import Supplier, Product
+from .views import productslistview
+client = Client()
 
-# Create your tests here.
+class ListMethodTests(TestCase):
+    def test_listing_products(self):
+        '''palauttaa statuskoodin 200'''
+        response = client.get(reverse(productslistview))
+        self.assertEqual(response.status_code, 200)
+
+class SupplierModelTests(TestCase):
+    def setUp(self):
+        Supplier.objects.create(companyname="Test Company", contactname="Jaakko Kulta", 
+        address="Kultatie 1", phone="12345567", email="jaakko@kulta.fi", country="Finland")
+
+    def test_added_supplier_exists(self):
+        '''Added supplier exists and can be searched'''
+        supplier = Supplier.objects.get(companyname="Test Company",)
+        self.assertEqual(supplier.address, "Kultatie 1")
+        self.assertEqual(supplier.country, "Finland")
+            
+
+
+class LaskinTests(TestCase):
+    def test_plus(self):
+        # testaa että numerot lasketaan yhteen oikein
+        self.assertEqual(plus(8, 2), 10)
+
+    def test_plus_string(self):
+        # testaa että numerot lasketaan yhteen oikein
+        self.assertEqual(plus("8", "2"), 10)
+
+    @unittest.expectedFailure
+    def test_plus_should_fail(self):
+        self.assertEqual(plus(7, 3), 9)
